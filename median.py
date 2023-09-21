9edc44b460acb2b68596744dbda5716253e4662d
Clara <clara.laoui@kcl.ac.uk>
2023-09-21 16:28:45 +0200

    Complete excercise

--- median.py
+++ median.py
@@ -1,12 +1,20 @@
+ 
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
 while True:
-    try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
-    else:
-        break
-print(numbers)
+
+print("Enter a list of numbers separated by commas: ")
+list = [float(value) for value in input().split(",")]
+length = len(list)
+list.sort()
+if length  % 2 == 0:
+        number1 = list[length // 2]
+        number2 = list[(length // 2) - 1]
+        print((number1 + number2)/ 2)
+else:
+        print(list[(length // 2)])
+
+
+
+
